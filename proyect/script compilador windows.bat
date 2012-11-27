7ceb481439bbc4a6ba16528499d2c7d1f5c915ae
SKATRAK <theSkatrak@gmail.com>
2012-11-27 17:52:16 +0000

    Modificados los archivos de proyecto para que compilen la nueva versión del programa.

--- proyect/script compilador windows.bat
+++ proyect/script compilador windows.bat
@@ -13,27 +13,35 @@ rem esta variable comprobará si hay algún error en el proceso de compilado.
 set error=false
 
 rem Argumentos para compilar y enlazar los códigos objeto. -mwindows para que no aparezca una consola.
-set CFLAGS=-I"..\include"
+set CFLAGS=-c -I"..\include"
 set LIBS=-L"..\lib" -mwindows -lmingw32 -lSDLmain -lSDL -lSDL_mixer -lSDL_image -lSDL_ttf
+set OBJECTS="..\bin\windows\obj\main.o" "..\bin\windows\obj\music.o" "..\bin\windows\obj\system.o" "..\bin\windows\obj\image.o" "..\bin\windows\obj\timekeeper.o" "..\bin\windows\obj\font.o" "..\bin\windows\obj\inifile.o" "..\bin\windows\obj\sfx.o" "..\bin\windows\obj\str_operations.o"
+set OUTPUT="..\bin\windows\SKATRAK Playground.exe"
 
 rem Compilamos los módulos objeto por separado
 echo Compilando modulos objeto...
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\main.o" "..\src\main.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\main.o" "..\src\main.cpp"
 if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\music.o" "..\src\music.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\music.o" "..\src\music.cpp"
 if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\system.o" "..\src\system.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\system.o" "..\src\system.cpp"
 if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\image.o" "..\src\image.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\image.o" "..\src\image.cpp"
 if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\timekeeper.o" "..\src\timekeeper.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\timekeeper.o" "..\src\timekeeper.cpp"
 if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" -c %CFLAGS% -o "..\bin\windows\obj\font.o" "..\src\font.cpp"
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\font.o" "..\src\font.cpp"
+if %errorlevel% neq 0 set error=true
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\inifile.o" "..\src\inifile.cpp"
+if %errorlevel% neq 0 set error=true
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\sfx.o" "..\src\sfx.cpp"
+if %errorlevel% neq 0 set error=true
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\str_operations.o" "..\src\str_operations.cpp"
 if %errorlevel% neq 0 set error=true
 
 rem Juntamos todos los módulos objeto en un solo ejecutable.
 echo Compilando ejecutable...
-"%gpp%\g++.exe" -O2 -o "..\bin\windows\SKATRAK Playground.exe" "..\bin\windows\obj\main.o" "..\bin\windows\obj\image.o" "..\bin\windows\obj\music.o" "..\bin\windows\obj\system.o" "..\bin\windows\obj\timekeeper.o" "..\bin\windows\obj\font.o" %LIBS%
+"%gpp%\g++.exe" -O2 -o %OUTPUT% %OBJECTS% %LIBS%
 if %errorlevel% neq 0 set error=true
 
 rem Decimos que la compilación ha acabado y pausamos el programa para que el usuario pueda leer los fallos en el caso de que los haya.
