12fbb2cfdb7560d618716143317a9111b06fbd29
SKATRAK <theSkatrak@gmail.com>
2012-11-27 18:24:35 +0000

    Comentarios de Doxygen añadidos a la clase sfx_t

--- src/sfx.cpp
+++ src/sfx.cpp
@@ -3,19 +3,34 @@
 #include <SDL/SDL_mixer.h>
 #include "../include/sfx.hpp"
 
+/**
+ * @brief Constructor. Inicializa el volumen a DEFAULT_SFX_VOLUME
+ */
 sfx_t::sfx_t(): sound(NULL), volume(DEFAULT_SFX_VOLUME)
 {
 }
 
+/**
+ * @brief Constructor. Inicializa el volumen a DEFAULT_SFX_VOLUME y abre un archivo de sonido.
+ * @param filename Ruta al archivo de sonido que se desea cargar.
+ * @see setSound
+ */
 sfx_t::sfx_t(string filename): sound(NULL), volume(DEFAULT_SFX_VOLUME) {
 	setSound(filename);
 }
 
+/**
+ * @brief Destructor. Libera la memoria ocupada por el archivo de sonido si fue cargado.
+ */
 sfx_t::~sfx_t(){
 	if(sound != NULL)
 		Mix_FreeChunk(sound);
 }
 
+/**
+ * @brief Carga en memoria un archivo de sonido.
+ * @param filename Nombre del fichero que se desea cargar.
+ */
 void sfx_t::setSound(string filename){
 	if(sound != NULL){
 		Mix_FreeChunk(sound);
@@ -26,6 +41,10 @@ void sfx_t::setSound(string filename){
 		fprintf(stderr, "No se ha podido cargar el sonido \"%s\"", filename.c_str());
 }
 
+/**
+ * @brief Cambia el volumen de reproducción del sonido.
+ * @param vol Valor del nuevo volumen que se desea asignar entre 0 y 128.
+ */
 void sfx_t::setVol(int vol){
 	if(vol >= 0 && vol <= 128)
 		Mix_VolumeChunk(sound, vol);
@@ -33,6 +52,9 @@ void sfx_t::setVol(int vol){
 		Mix_VolumeChunk(sound, DEFAULT_SFX_VOLUME);
 }
 
+/**
+ * @brief Reproduce el efecto de sonido una vez si se ha cargado
+ */
 void sfx_t::play(){
 	if(sound != NULL){
 		Mix_PlayChannel(-1, sound, 0);
