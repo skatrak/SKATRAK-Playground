7c4d9576fc857dfdeac52b61bfdc6abf8443deec
SKATRAK <theSkatrak@gmail.com>
2012-12-01 19:12:32 +0000

    main() modificado para hacer la introducción al juego

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../include/SKATRAK_PLAYGROUND.hpp"
+#include "../include/shared_attributes.hpp"
 
 /* Definiciones de las rutas por defecto de los recursos */
 const char* MUS_PATH = "../../resources/sound/";
@@ -7,68 +8,101 @@ const char* FONT_PATH = "../../resources/fonts/";
 const char* IMG_PATH = "../../resources/images/";
 const char* INI_PATH = "../../resources/settings/";
 
-inifile_t pruebaIni("prueba.ini");
-system_t sistema(1024, 768, 32);	// La variable sistema se comparte entre todos los minijuegos y permanece igual para todos ellos.
+/* Prototipos de los menús de los distintos minijuegos */
+returnVal mainMenu();
+//returnVal main3enRaya();
+//returnVal mainConecta4();
+//returnVal mainPong();
+//[...]
+
+system_t* sistema = NULL;			// La variable sistema se comparte entre todos los minijuegos y permanece igual para todos ellos.
 music_t* musica = NULL;				// La variable musica se comparte por todos los minijuegos y cada uno puede tener su propia lista de reproducción.
 
 int main(int argc, char* argv[]){
-	// Ponemos un icono a la ventana
-	sistema.setIcon("icono_prueba.png");
+	// Creamos la variable sistema (Iniciamos SDL)
+	sistema = new system_t(1024, 768, 32);
+	sistema->setIcon("icono_prueba.png");
+
 	// Cargamos 3 pistas de música para que reproduzcan en el fondo
 	musica = new music_t(3);
 	musica->setVol(128);
 	musica->setTrack(0, "track01.ogg");
 	musica->setTrack(1, "track02.ogg");
 	musica->setTrack(2, "track03.ogg");
-	// Cargamos y mostramos por pantalla una imagen de prueba
-	image_t prueba("icono_prueba.png");
-	// Creamos el temporizador
+
+	// Imágenes para mostrar en el menú inicio
+	SDL_Surface* screen = sistema->scr();
+	image_t fondo("Fondo_inicio_prueba.png");
+	font_t nombreJuego("font01.ttf");
+	nombreJuego.setSize(72);
+	nombreJuego.setText("SKATRAK Playground");
+	font_t empezar("font01.ttf");
+	empezar.setSize(32);
+	empezar.setText("Pulse una tecla para comenzar");
 	timekeeper_t temporizador;
-	// Creamos dos fuentes, les asignamos un tamaño, un estilo y un texto
-	font_t fuente;
-	font_t fuente2("font01.ttf");
 
-	fuente.open("font01.ttf");
-	fuente.setSize(64);
-	fuente2.setSize(32);
-	fuente.setStyle(TTF_STYLE_UNDERLINE | TTF_STYLE_ITALIC);
-	fuente2.setStyle(TTF_STYLE_BOLD);
-	fuente.setText(pruebaIni.readString("Other", "Text1"));
-	fuente2.setText(pruebaIni.readString("Other", "Text2"));
-	fuente2.setColor(255, 255, 0);
-
+	// Variables para controlar el game loop
 	SDL_Event event;
 	bool salir = false;
 
 	// Comenzamos a reproducir la música
 	musica->play();
-	// GAME LOOP
+
+	double alpha = 0.0;
+	while((int)alpha < SDL_ALPHA_OPAQUE){
+		temporizador.refresh();
+		nombreJuego.setAlpha((int)alpha);		// Debería ir de transparente a visible, pero no hace nada la transparencia
+		fondo.blit(0, 0, screen);
+		nombreJuego.blit((int)(screen->w / 2)- (int)(nombreJuego.width() / 2), (int)(screen->h / 2 - nombreJuego.height()), screen);
+		sistema->update();
+		temporizador.waitFramerate(30);
+		alpha += 2;
+		SDL_PollEvent(NULL);
+	}
+
+	bool alphaAdd = false;
 	while(!salir){
-		// Reiniciamos el temporizador en cada ciclo del game loop
+		// Reiniciamos el temporizador en cada ciclo
 		temporizador.refresh();
 		// Gestionamos los eventos
 		while(SDL_PollEvent(&event)){
 			switch(event.type){
 				case SDL_KEYDOWN:
-					if(event.key.keysym.sym == SDLK_ESCAPE) salir = true;
-					if(event.key.keysym.sym == SDLK_RIGHT) nextTrack();
+					if(mainMenu() == ERROR)
+						fprintf(stderr, "Se ha salido del programa con un error.\n");
+					salir = true;
 					break;
 				case SDL_QUIT:
 					salir = true;
 			}
 		}
-		// Ponemos un color aleatorio a las letras en cada fotograma
-		fuente.setColor(rand() % 256, rand() % 256, rand() % 256);
+		// Para dar una sensación de texto parpadeante (Cuando funcionen las transparencias)
+		if(alphaAdd)
+			alpha += 0.5;
+		else
+			alpha -= 0.5;
+		if((int)alpha == SDL_ALPHA_TRANSPARENT) alphaAdd = true;
+		else if((int)alpha == SDL_ALPHA_OPAQUE) alphaAdd = false;
+		empezar.setAlpha((int)alpha);
+
 		// Imprimimos por pantalla todo lo que haga falta e intercambiamos los buffers de vídeo
-		fuente.blit(200, 200, sistema.scr());
-		fuente2.blit(200 + (int)fuente.width()/2, 200 + fuente.height(), sistema.scr());	// Colocamos esta línea debajo de la anterior y separada horizontalmente
-		prueba.blit(100, 100, sistema.scr());
-		sistema.update();
+		fondo.blit(0, 0, screen);
+		nombreJuego.blit((int)(screen->w / 2)- (int)(nombreJuego.width() / 2), (int)(screen->h / 2 - nombreJuego.height()), screen);
+		empezar.blit((int)(screen->w / 2)- (int)(empezar.width() / 2), (int)(3 * screen->h / 4 - empezar.height()), screen);
+		sistema->update();
+
 		// Fijamos los FPS a 30
 		temporizador.waitFramerate(30);
 	}
-	// Paramos la música antes de salir. No hay que liberar memoria porque se libera en los destructores de las clases
+
+	// Paramos la música antes de salir y liberamos recursos
 	musica->halt();
 	printf("El programa ha estado abierto %d segundos y ha imprimido %d fotogramas.\n", (int)temporizador.elapsed()/1000, temporizador.renderedFrames());
+	if(musica != NULL)
+		delete musica;
+	if(sistema != NULL)
+		delete sistema;
 	return 0;
 }
+
+returnVal mainMenu(){ return EXIT; }
