fa07eaa4dfb885ac22627e151531007ff9d36367
SKATRAK <theSkatrak@gmail.com>
2012-12-06 23:32:40 +0000

    Menú principal de prueba utilizando la clase menu_t

--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -21,5 +21,42 @@
 
 #include "../include/SKATRAK_PLAYGROUND.hpp"
 #include "../include/shared_attributes.hpp"
+#include "../include/menu.hpp"
 
-returnVal mainMenu(){ return EXIT; }
+returnVal defaultCallback(void* data){
+	return EXIT;
+}
+
+returnVal mainMenu(){
+	menu_t menu(4);
+	for(int i = 0; i < 4; i++)
+		menu.setOpt(i, &defaultCallback);
+	menu.setTexts("font01.ttf", 48);
+	menu.setText(0, "op1");
+	menu.setText(1, "    op2");
+	menu.setText(2, "        op3");
+	menu.setText(3, "            op4");
+	menu.setBackground("Fondo_inicio_prueba.png");
+	menu.setImage("marcador_prueba.png");
+
+	SDL_Event event;
+	SDL_Surface* screen = sistema->scr();
+	timekeeper_t timer;
+	bool salir = false;
+	while(!salir){
+		timer.refresh();
+		while(SDL_PollEvent(&event)){
+			switch(menu.update(&event)){
+				case EXIT:
+					salir = true;
+					break;
+				case ACTUAL_MENU:
+					break;
+			}
+		}
+		menu.blit(screen);
+		sistema->update();
+		timer.waitFramerate(30);
+	}
+	printf("Has estado %d segundos en el menú principal.\n", (int)(timer.elapsed()/1000));
+}
