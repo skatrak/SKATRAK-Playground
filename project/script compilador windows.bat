8f7258c55fa52feee2396ed499ee875adcd5bee0
SKATRAK <theSkatrak@gmail.com>
2012-12-26 20:10:39 +0000

    Actualizados archivos de proyecto para compilar

--- project/script compilador windows.bat
+++ project/script compilador windows.bat
@@ -6,8 +6,11 @@ rem ESCRITO POR SERGIO M. AFONSO FUMERO (theSkatrak@gmail.com)
 rem Preguntamos al usuario la dirección del compilador GNU
 set /p gpp="Introduce la ruta completa o relativa al directorio donde se encuentra g++ (Por defecto 'C:\MinGW\bin'): "
 
-rem Comprobamos que la respuesta no esté vacía
-if "%gpp%" equ "" echo Para compilar, debes descargar e instalar primero MinGW e indicar aqui la ruta de su carpeta bin. && pause>nul && exit
+rem Si la respuesta está vacía ponemos la ruta por defecto
+if "%gpp%" equ "" set gpp=C:\MinGW\bin
+
+rem Comprobamos que el compilador está en esa ruta
+if not exist "%gpp%\g++.exe" echo No se encuentra g++.exe en %gpp%. Recuerda que hay que instalar MinGW para poder compilar usando este método. && pause>nul && exit
 
 rem esta variable comprobará si hay algún error en el proceso de compilado.
 set error=false
@@ -15,7 +18,7 @@ set error=false
 rem Argumentos para compilar y enlazar los códigos objeto. -mwindows para que no aparezca una consola.
 set CFLAGS=-Wall -c -I"..\include"
 set LIBS=-L"..\lib\windows" -mwindows -lmingw32 -lSDLmain -lSDL -lSDL_mixer -lSDL_image -lSDL_ttf
-set OBJECTS="..\bin\windows\obj\main.o" "..\bin\windows\obj\music.o" "..\bin\windows\obj\system.o" "..\bin\windows\obj\image.o" "..\bin\windows\obj\timekeeper.o" "..\bin\windows\obj\font.o" "..\bin\windows\obj\inifile.o" "..\bin\windows\obj\sfx.o" "..\bin\windows\obj\str_operations.o" "..\bin\windows\obj\menu.o" "..\bin\windows\obj\mainMenu.o" "..\bin\windows\obj\mainConecta4.o" "..\bin\windows\obj\Game_Conecta4.o"
+set OBJECTS="..\bin\windows\obj\main.o" "..\bin\windows\obj\music.o" "..\bin\windows\obj\system.o" "..\bin\windows\obj\image.o" "..\bin\windows\obj\timekeeper.o" "..\bin\windows\obj\font.o" "..\bin\windows\obj\inifile.o" "..\bin\windows\obj\sfx.o" "..\bin\windows\obj\str_operations.o" "..\bin\windows\obj\menu.o" "..\bin\windows\obj\mainMenu.o" "..\bin\windows\obj\tablero.o" "..\bin\windows\obj\mainConecta4.o" "..\bin\windows\obj\Game_Conecta4.o"
 set OUTPUT="..\bin\windows\SKATRAK Playground.exe"
 
 rem Compilamos los módulos objeto por separado
@@ -43,6 +46,8 @@ if %errorlevel% neq 0 set error=true
 "%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\mainMenu.o" "..\src\mainMenu.cpp"
 if %errorlevel% neq 0 set error=true
 rem Conecta 4
+"%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\tablero.o" "..\src\conecta4\tablero.cpp"
+if %errorlevel% neq 0 set error=true
 "%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\mainConecta4.o" "..\src\conecta4\mainConecta4.cpp"
 if %errorlevel% neq 0 set error=true
 "%gpp%\g++.exe" %CFLAGS% -o "..\bin\windows\obj\Game_Conecta4.o" "..\src\conecta4\Game_Conecta4.cpp"
