bb8222825f32a5b961dccefe4ac0f13a4f06c64c
SKATRAK <theSkatrak@gmail.com>
2013-01-28 19:05:03 +0000

    Clase snakePiece añadida.
    Esta clase representa una posición (x,y) de un eslabón de la serpiente
    preparada para ser utilizada como elemento de una LSE.

---
+++ include/snake/snakePiece.hpp
@@ -0,0 +1,69 @@
+/*
+ *  This file is part of SKATRAK Playground.
+ *
+ *  SKATRAK Playground is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/> or
+ *  write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ *  Floor, Boston, MA 02110-1301 USA
+ *
+ *  Sergio M. Afonso Fumero <theSkatrak@gmail.com>
+ */
+
+#ifndef __SNAKEPIECE_T__
+#define __SNAKEPIECE_T__
+
+// Direcciones de movimiento de la serpiente
+enum Direction {
+	MOVE_UP,
+	MOVE_DOWN,
+	MOVE_LEFT,
+	MOVE_RIGHT
+};
+
+/**
+ * @class snakePiece_t
+ * @brief Representa un elemento para una lista simplemente enlazada de 'eslabones' de la serpiente.
+ */
+class snakePiece_t {
+	private:
+		int x, y;
+	public:
+		snakePiece_t* next;
+		snakePiece_t(void);
+		snakePiece_t(int posX, int posY, snakePiece_t* nextPiece = NULL);
+		snakePiece_t(snakePiece_t& copy, snakePiece_t* nextPiece = NULL);
+		~snakePiece_t(void){}
+		int posX(void){ return x; }
+		int posY(void){ return y; }
+		void setPos(int posX, int posY);
+		void move(Direction direction);
+};
+
+/**
+ * @fn snakePiece_t::~snakePiece_t
+ * @brief Destructor.
+ */
+
+/**
+ * @fn snakePiece_t::posX
+ * @brief Devuelve la posición en el eje X actual del eslabón.
+ * @return Posición en el eje X.
+ */
+
+/**
+ * @fn snakePiece_t::posY
+ * @brief Devuelve la posición en el eje Y actual del eslabón.
+ * @return Posición en el eje Y.
+ */
+
+#endif
---
+++ src/snake/snakePiece.cpp
@@ -0,0 +1,80 @@
+/*
+ *  This file is part of SKATRAK Playground.
+ *
+ *  SKATRAK Playground is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/> or
+ *  write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ *  Floor, Boston, MA 02110-1301 USA
+ *
+ *  Sergio M. Afonso Fumero <theSkatrak@gmail.com>
+ */
+
+#include <SKATRAK_PLAYGROUND.hpp>
+#include <snake/snakePiece.hpp>
+
+/**
+ * @brief Constructor por defecto. Inicializa las variables de la clase con un valor conocido.
+ */
+snakePiece_t::snakePiece_t(): x(-1), y(-1), next(NULL)
+{
+}
+
+/**
+ * @brief Constructor. Inicializa la posición del eslabón.
+ * @posX Posición del eslabón en el eje X.
+ * @posY Posición del eslabón en el eje Y.
+ * @param (OPCIONAL) Puntero al siguiente elemento de la lista.
+ */
+snakePiece_t::snakePiece_t(int posX, int posY, snakePiece_t* nextPiece): x(posX), y(posY), next(nextPiece)
+{
+}
+
+/**
+ * @brief Constructor de copia.
+ * @param copy Referencia al objeto snakePiece_t al que copiar.
+ * @param nextPiece (OPCIONAL) Puntero al siguiente elemento de la lista.
+ */
+snakePiece_t::snakePiece_t(snakePiece_t& copy, snakePiece_t* nextPiece): x(copy.x), y(copy.y), next(nextPiece)
+{
+}
+
+/**
+ * @brief Sitúa el eslabón en una posición.
+ * @param posX Posición del eslabón en el eje X.
+ * @param posY Posición del eslabón en el eje Y.
+ */
+void snakePiece_t::setPos(int posX, int posY){
+	x = posX;
+	y = posY;
+}
+
+/**
+ * @brief Mueve el eslabón una posición hacia una dirección.
+ * @param direction Dirección hacia la que mover el eslabón
+ */
+void snakePiece_t::move(Direction direction){
+	switch(direction){
+	case MOVE_UP:
+		y--;
+		break;
+	case MOVE_DOWN:
+		y++;
+		break;
+	case MOVE_LEFT:
+		x--;
+		break;
+	case MOVE_RIGHT:
+		x++;
+		break;
+	}
+}
