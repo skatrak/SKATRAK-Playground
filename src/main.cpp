fdd3b0f78b62ae73b7e7d36eb395022cb58401f6
SKATRAK <theSkatrak@gmail.com>
2013-01-28 16:27:50 +0000

    Eliminamos la prueba de lectura de archivos ini (FUNCIONA).
    Eliminado un bug que no había detectado: Se cerraba SDL antes de liberar las variables
    que estaban en la pila, por lo que al intentar liberarse éstas y estar SDL cerrado,
    había un fallo de segmentación. Reparado poniendo la pantalla inicial en una función
    aparte.

--- src/main.cpp
+++ src/main.cpp
@@ -31,14 +31,12 @@ const char* INI_PATH = "../../resources/settings/";
 
 /* Prototipo del menú principal */
 returnVal mainMenu();
-void tryIni(void);
+void startScreen(void);
 
 system_t* sistema = NULL;			// La variable sistema se comparte entre todos los minijuegos y permanece igual para todos ellos.
 music_t* musica = NULL;				// La variable musica se comparte por todos los minijuegos y cada uno puede tener su propia lista de reproducción.
 
 int main(int argc, char* argv[]){
-	// Prueba de lectura en un archivo .ini
-	tryIni();
 	// Creamos la variable sistema (Iniciamos SDL)
 	sistema = new system_t(1024, 768, 32);
 	sistema->setIcon("icono_prueba.png");
@@ -54,18 +52,35 @@ int main(int argc, char* argv[]){
 	SDL_EnableKeyRepeat(250, 75);
 
 	// Imágenes para mostrar en el menú inicio
-	SDL_Surface* screen = sistema->scr();
-	if(screen == NULL){
+	if(sistema->scr() == NULL){
 		fprintf(stderr, "La pantalla no se ha iniciado. Saliendo del programa...\n");
 		return 1;
 	}
-	image_t fondo("Fondo_inicio_prueba.png");
+
+	// Pantalla inicial
+	startScreen();
+
+	// Paramos la música antes de salir y liberamos recursos
+	musica->halt();
+	if(musica != NULL)
+		delete musica;
+	if(sistema != NULL)
+		delete sistema;
+	return 0;
+}
+
+void startScreen(){
+	// Pantalla
+	SDL_Surface* screen = sistema->scr();
+
+	// Elementos de la pantalla
+	image_t fondo("fondo_inicio_prueba.png");
 	font_t nombreJuego("BOOTERFF.ttf");
 	nombreJuego.setSize(72);
 	nombreJuego.setText("SKATRAK Playground");
 	font_t empezar("BOOTERFF.ttf");
 	empezar.setSize(32);
-	empezar.setText("Pulse una tecla para comenzar");
+	empezar.setText("Pulse una tecla para empezar");
 	timekeeper_t temporizador;
 
 	// Variables para controlar el game loop
@@ -123,60 +138,4 @@ int main(int argc, char* argv[]){
 		// Fijamos los FPS a 30
 		temporizador.waitFramerate(30);
 	}
-
-	// Paramos la música antes de salir y liberamos recursos
-	musica->halt();
-	if(musica != NULL)
-		delete musica;
-	if(sistema != NULL)
-		delete sistema;
-	return 0;
-}
-
-/* FUNCIÓN PARA PROBAR EL FUNCIONAMIENTO DE LOS ARCHIVOS .INI */
-void tryIni(void){
-	inifile_t prueba("prueba.ini");
-	string cadenas[3];
-	int enteros[4];
-	double decimales[3];
-	bool booleanos[4];
-	// Lectura de cadenas
-	cadenas[0] = prueba.readString("Strings", "cad1");
-	cadenas[1] = prueba.readString("Strings", "cad2");
-	cadenas[2] = prueba.readString("Strings", "cad3");
-	// Lectura de enteros
-	enteros[0] = prueba.readInt("Integers", "numero1");
-	enteros[1] = prueba.readInt("Integers", "numero2");
-	enteros[2] = prueba.readInt("Integers", "numero3");
-	enteros[3] = prueba.readInt("Integers", "numero4");
-	// Lectura de decimales
-	decimales[0] = prueba.readDouble("Floats", "numero1");
-	decimales[1] = prueba.readDouble("Floats", "numero2");
-	decimales[2] = prueba.readDouble("Floats", "numero3");
-	// Lectura de booleanos
-	booleanos[0] = prueba.readBool("Bools", "booleano1");
-	booleanos[1] = prueba.readBool("Bools", "booleano2");
-	booleanos[2] = prueba.readBool("Bools", "booleano3");
-	booleanos[3] = prueba.readBool("Bools", "booleano4");
-	// Comprobación de los resultados
-	int i;
-	printf("PRUEBA DE LECTURA EN .INI:\n");
-	for(i = 0; i < 3; i++)
-		printf("Cadena %d: %s\n", i+1, cadenas[i].c_str());
-	printf("\n");
-	for(i = 0; i < 4; i++)
-		printf("Entero %d: %d\n", i+1, enteros[i]);
-	printf("\n");
-	for(i = 0; i < 3; i++)
-		printf("Decimal %d: %lf\n", i+1, decimales[i]);
-	printf("\n");
-	for(i = 0; i < 4; i++){
-		printf("Booleano %d: ", i+1);
-		if(booleanos[i])
-			printf("Verdadero");
-		else
-			printf("Falso");
-		printf("\n");
-	}
-	printf("\n");
 }
