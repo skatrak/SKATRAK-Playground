e9f51931c3ae88c9b563ba0e44f4074c29580b3c
SKATRAK <theSkatrak@gmail.com>
2013-01-29 00:12:14 +0000

    Archivos de proyecto actualizados para compilar la última versión del programa
    El script compilador de Windows está bastante más optimizado que antes. ¿Alguna idea para hacer lo mismo en Linux?

--- project/script compilador windows.bat
+++ project/script compilador windows.bat
@@ -22,52 +22,32 @@ set LIBS=-L"..\lib\windows" -mwindows -lmingw32 -lSDLmain -lSDL -lSDL_mixer -lSD
 rem Directorios importantes
 set OBJDIR=..\bin\windows\obj
 set SRCDIR=..\src
-set OBJECTS_GEN="%OBJDIR%\main.o" "%OBJDIR%\music.o" "%OBJDIR%\system.o" "%OBJDIR%\image.o" "%OBJDIR%\timekeeper.o" "%OBJDIR%\font.o" "%OBJDIR%\inifile.o" "%OBJDIR%\sfx.o" "%OBJDIR%\str_operations.o" "%OBJDIR%\menu.o" "%OBJDIR%\mainMenu.o" "%OBJDIR%\messagebox.o"
-set OBJECTS_C4="%OBJDIR%\tablero.o" "%OBJDIR%\mainConecta4.o" "%OBJDIR%\gameConecta4.o" "%OBJDIR%\messagesConecta4.o"
 set OUTPUT="..\bin\windows\SKATRAK Playground.exe"
+
+rem Listado de módulos objeto
+set OBJECTS_GEN="%OBJDIR%\system.o" "%OBJDIR%\main.o" "%OBJDIR%\music.o" "%OBJDIR%\image.o" "%OBJDIR%\timekeeper.o" "%OBJDIR%\font.o" "%OBJDIR%\inifile.o" "%OBJDIR%\sfx.o" "%OBJDIR%\str_operations.o" "%OBJDIR%\menu.o" "%OBJDIR%\mainMenu.o" "%OBJDIR%\messagebox.o"
+set OBJECTS_C4="%OBJDIR%\mainConecta4.o" "%OBJDIR%\tablero.o" "%OBJDIR%\gameConecta4.o" "%OBJDIR%\messagesConecta4.o"
+set OBJECTS_SNK="%OBJDIR%\mainSnake.o" "%OBJDIR%\snakePiece.o" "%OBJDIR%\snake.o"
 
 rem Compilamos los módulos objeto por separado
 echo Compilando modulos objeto...
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\main.o" "%SRCDIR%\main.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\music.o" "%SRCDIR%\music.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\system.o" "%SRCDIR%\system.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\image.o" "%SRCDIR%\image.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\timekeeper.o" "%SRCDIR%\timekeeper.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\font.o" "%SRCDIR%\font.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\inifile.o" "%SRCDIR%\inifile.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\sfx.o" "%SRCDIR%\sfx.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\str_operations.o" "%SRCDIR%\str_operations.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\menu.o" "%SRCDIR%\menu.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\mainMenu.o" "%SRCDIR%\mainMenu.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\messagebox.o" "%SRCDIR%\messagebox.cpp"
-if %errorlevel% neq 0 set error=true
-rem Conecta 4
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\tablero.o" "%SRCDIR%\conecta4\tablero.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\mainConecta4.o" "%SRCDIR%\conecta4\mainConecta4.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\gameConecta4.o" "%SRCDIR%\conecta4\gameConecta4.cpp"
-if %errorlevel% neq 0 set error=true
-"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\messagesConecta4.o" "%SRCDIR%\conecta4\messagesConecta4.cpp"
-if %errorlevel% neq 0 set error=true
+for /R %%i in (%SRCDIR%\*.cpp) do (call :compile "%%~nxi" %SRCDIR%)
+for /R %%i in (%SRCDIR%\conecta4\*.cpp) do (call :compile "%%~nxi" %SRCDIR%\conecta4)
+for /R %%i in (%SRCDIR%\snake\*.cpp) do (call :compile "%%~nxi" %SRCDIR%\snake)
 
 rem Juntamos todos los módulos objeto en un solo ejecutable.
 echo Compilando ejecutable...
-"%gpp%\g++.exe" -s -O2 -o %OUTPUT% %OBJECTS_GEN% %OBJECTS_C4% %LIBS%
+"%gpp%\g++.exe" -s -O2 -o %OUTPUT% %OBJECTS_GEN% %OBJECTS_C4% %OBJECTS_SNK% %LIBS%
 if %errorlevel% neq 0 set error=true
 
 rem Decimos que la compilación ha acabado y pausamos el programa para que el usuario pueda leer los fallos en el caso de que los haya.
 echo Compilacion finalizada.
 if "%error%"=="true" (echo Han habido errores en la compilacion.) else (echo La compilacion se ha realizado satisfactoriamente.)
 pause>nul
+exit
+
+:compile
+set filename=%~n1.o
+echo Compilando "%OBJDIR%\%filename%"...
+"%gpp%\g++.exe" %CFLAGS% -o "%OBJDIR%\%filename%" "%~2\%~1"
+if %errorlevel% neq 0 set error=true
